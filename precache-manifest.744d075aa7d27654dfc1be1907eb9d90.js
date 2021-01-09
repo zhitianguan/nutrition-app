c39494b978aedcb7800cdc102a3a85a3d82a423e
Zhitian Guan <zguan.university@gmail.com>
2021-01-09 15:02:32 -0500

    Updates

---
+++ precache-manifest.744d075aa7d27654dfc1be1907eb9d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b335d52a5b453048fd62a238b4689be",
+    "url": "/nutrition-app/index.html"
+  },
+  {
+    "revision": "16aec33a1f1532c04292",
+    "url": "/nutrition-app/static/css/main.f9935f13.chunk.css"
+  },
+  {
+    "revision": "c133ea823bd88eb924b2",
+    "url": "/nutrition-app/static/js/2.ab88b7e2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/nutrition-app/static/js/2.ab88b7e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16aec33a1f1532c04292",
+    "url": "/nutrition-app/static/js/main.57e0465e.chunk.js"
+  },
+  {
+    "revision": "5ee9e98ed62c4291a694",
+    "url": "/nutrition-app/static/js/runtime-main.148348f1.js"
+  }
+]);
